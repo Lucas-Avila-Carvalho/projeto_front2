274bc55167c52ca45e5eaf4720d4ce19f491c6cc
Lucas Carvalho <lucasavilacarvalho5@gmail.com>
2025-09-08 19:19:20 -0300

    react

---
+++ cstsi_dfe2_202502_js_react-main/topico02/03-vite-reactjs-primeiro-site/src/componets/Avatar.jsx
@@ -0,0 +1,47 @@
+ const users = [{
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+  wiki: 'https://pt.wikipedia.org/wiki/Hedy_Lamarr',
+},
+{
+  name: 'Ada Lovelace',
+  imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/0f/Ada_lovelace.jpg',
+  imageSize: 90,
+  wiki: 'https://pt.wikipedia.org/wiki/Ada_Lovelace',
+},
+{
+  name: 'Isaac Newton',
+  imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Portrait_of_Sir_Isaac_Newton%2C_1689_%28brightened%29.jpg/800px-Portrait_of_Sir_Isaac_Newton%2C_1689_%28brightened%29.jpg',
+  imageSize: 90,
+  wiki: 'https://pt.wikipedia.org/wiki/Isaac_Newton',
+}];
+
+ function Profile({ user }) {
+  return <>
+
+    <Title content={user.name} link={user.wiki} />
+    <Avatar user={user} />
+  </>
+}
+ function Avatar({ user }) {
+  return (
+    <a href={user.wiki}>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Foto de ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </a>
+  );
+}
+
+ function Title({ content, link }) {
+  return <a href={link}>
+    <h1>{content}</h1>
+  </a>
+}
